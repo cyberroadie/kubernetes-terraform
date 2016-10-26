96d756485b4450cc3a33c8963592f2bbd4a18097
Olivier Van Acker <cyberroadie@gmail.com>
2016-10-26 18:03:53 +0100

    correction variable names, adding {}

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,11 +1,19 @@
 output "vpc_id" {
-    value = "$aws_vpc.orchestration_vpc.id" 
+    value = "${aws_vpc.orchestration_vpc.id}" 
 }
 
 output "dhcp_id_id" {
-    value = "$aws_vpc_dhcp_options.orchestration_dhcp.id" 
+    value = "${aws_vpc_dhcp_options.orchestration_dhcp.id}" 
 }
 
 output "subnet_id" {
-    value = "$aws_subnet.orchestration_subnet.id" 
+    value = "${aws_subnet.orchestration_subnet.id}" 
+}
+
+output "controller_ips" {
+    value = ["${aws_instance.orchestration_control.*.public_ip}"]
+}
+
+output "worker_ips" {
+    value = ["${aws_instance.orchestration_worker.*.public_ip}"]
 }
